2e5d6ab42fabf189786ce0ae6ccbb78fc98f07b5
Sergey <karandeev73@gmail.com>
2022-02-25 21:27:45 +0300

    feat: Kivy Float Layout

---
+++ FloatLayout/float_layout.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+# Designate Uur .kv design file
+Builder.load_file('float_layout.kv')
+
+class MyLayout(Widget):
+    pass
+
+class SomeApp(App):
+    def build(self):
+        return MyLayout()
+
+if __name__ == '__main__':
+    SomeApp().run()
